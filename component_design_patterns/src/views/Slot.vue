9a2067922eada44842c6ebdfc3453fe2ff472b90
Jaehyeokk <lionjk92@naver.com>
2021-06-23 22:57:47 +0900

    Design pattern - Component wid Slots

--- component_design_patterns/src/views/Slot.vue
+++ component_design_patterns/src/views/Slot.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    Slot
+    <ul>
+      <Slot-item>item 1</Slot-item>
+      <Slot-item>item 2 <button>button</button></Slot-item>
+      <Slot-item>item 3 <img src="../assets/logo.png" alt="logo"/></Slot-item>
+      <Slot-item style="color: blue;">item 4</Slot-item>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {};
+import SlotItem from "../components/SlotItem.vue";
+export default {
+  components: {
+    SlotItem,
+  },
+};
 </script>
 
 <style></style>
