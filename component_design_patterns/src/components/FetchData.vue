a57a8c15f831989cfd3d2c23407452f5cd8abd9a
Jaehyeokk <lionjk92@naver.com>
2021-06-24 23:04:59 +0900

    Design pattern - Renderless Component

---
+++ component_design_patterns/src/components/FetchData.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <slot :response="response" :loading="loading"></slot>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  props: ["url"],
+  data() {
+    return {
+      response: null,
+      loading: true,
+    };
+  },
+  created() {
+    axios
+      .get(this.url)
+      .then((response) => {
+        this.response = response.data;
+        this.loading = false;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
+
+<style></style>
--- component_design_patterns/src/views/Renderless.vue
+++ component_design_patterns/src/views/Renderless.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    Renderless
+    <fetch-data url="https://jsonplaceholder.typicode.com/todos/1">
+      <template v-slot="{ response, loading }">
+        <div v-if="loading">loading...</div>
+        <div v-if="!loading">{{ response }}</div>
+      </template>
+    </fetch-data>
   </div>
 </template>
 
 <script>
-export default {};
+import FetchData from "../components/FetchData.vue";
+export default {
+  components: {
+    FetchData,
+  },
+};
 </script>
 
 <style></style>
