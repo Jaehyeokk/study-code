14885645cf5469e8f37a0656f94d091342bf7454
Jaehyeokk <lionjk92@naver.com>
2021-10-07 16:16:43 +0900

    TypeScript - Adding type definition

--- typescript/1_type_basic.ts
+++ typescript/1_type_basic.ts
@@ -10,5 +10,17 @@ let num: number = 10;
 
 let arr: Array<number> = [1,2,3];
 let heroes: Array<string> = ['Capt', 'Thor', 'Hulk']
-
 let items: number[] = [1,2,3];
+
+// TS 튜플
+let address: [string, number] = ['gangnam', 100]
+
+// TS 객체
+let obj: object = {};
+let person: {name: string, age: number} = {
+  name: 'capt',
+  age: 100
+}
+
+// TS 진위
+let show: boolean = true;
