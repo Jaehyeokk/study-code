bef9e0092ac83a0567b201c79af2ac4ad1e84ac9
Jaehyeokk <lionjk92@naver.com>
2021-10-28 21:57:48 +0900

    Vue - Update Vue-mall

---
+++ vue/vue-mall/js/app.js
@@ -0,0 +1,65 @@
+import SearchModel from './models/SearchModel.js'
+import KeywordsModel from './models/KeywordModel.js'
+import HistoryModel from './models/HistoryModel.js'
+
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      query: '',
+      submited: false,
+      tabs: ['추천 검색어', '최근 검색어'],
+      selectedTab: '',
+      keywords: [],
+      history: [],
+      searchResult: [],
+    }
+  },
+  created() {
+    this.selectedTab = this.tabs[0]
+    this.fetchKeyword()
+    this.fetchHistory()
+  },
+  methods: {
+    onSubmit(e) {
+      this.search()
+    },
+    onReset() {
+      this.query = ''
+      this.submited = false
+      this.searchResult = []
+    },
+    onKeyup() {
+      if(!this.query.length) this.onReset()
+    },
+    onClickTab(tab) {
+      this.selectedTab = tab
+    },
+    onClickKeyword(keyword) {
+      this.query = keyword
+      this.search(keyword)
+    },
+    onClickRemoveHistory(keyword) {
+      HistoryModel.remove(keyword)
+      this.fetchHistory()
+    },
+    search() {
+      SearchModel.list().then(data => {
+        this.submited = true
+        this.searchResult = data
+      })
+      HistoryModel.add(this.query)
+      this.fetchHistory()
+    },
+    fetchKeyword() {
+      KeywordsModel.list().then(data => {
+        this.keywords = data
+      })
+    },
+    fetchHistory() {
+      HistoryModel.list().then(data => {
+        this.history = data
+      })
+    }
+  }
+})
